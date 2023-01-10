ff785af863f59ad024400860539cdf22f315221f
unsivil <unsivilaudio@gmail.com>
2023-01-10 05:41:18 -0700

    initial commit

---
+++ pages/new-meetup/[meetupId].js
@@ -0,0 +1,14 @@
+// import { Fragment } from 'react';
+
+// function MeetupDetails() {
+// 	return (
+// 		<Fragment>
+// 			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Branicki_Palace_in_Bia%C5%82ystok.jpg/360px-Branicki_Palace_in_Bia%C5%82ystok.jpg" alt="" />
+// 			<h1>A firs meetup</h1>
+// 			<address>some street 5</address>
+// 			<p>some text here</p>
+// 		</Fragment>
+// 	);
+// }
+
+// export default MeetupDetails;
